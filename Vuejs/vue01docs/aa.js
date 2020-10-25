2270e13d97d26797ae75002e919ce877adcbecb3
abc <abc>
2020-10-25 11:24:09 +0900

    2020-10-25

---
+++ Vuejs/vue01docs/aa.js
@@ -0,0 +1,27 @@
+//import axios from 
+;
+
+new Vue({
+    /* edtmc^2w */
+    el: "",
+    data: {
+
+    },
+    //template: ``,
+    methods: {
+
+    },
+    components: { }, 
+    computed: {
+
+    },
+    watch: {
+
+    },
+    mounted: function() {
+        console.log("mounted");
+    },
+    updated: function() {
+        console.log("updated");
+    },
+});
